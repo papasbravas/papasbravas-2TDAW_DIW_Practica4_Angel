1fd94c922d18bc4ea0668eded373602e0dbcd61a
papasbravas <aeg2809@gmail.com>
2024-11-25 18:43:34 +0100

    Blog Hecho (Falta Responsive en algunas partes)

--- animaciones.js
+++ animaciones.js
@@ -5,3 +5,32 @@ function mostrar(element) {
     element.querySelector('span').textContent = escondido ? '-' : '+';
 }
 
+const carrusel = document.querySelector('.tendencias-carrusel');
+    const items = document.querySelectorAll('.tendencia-item');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    let currentIndex = 0;
+
+    function moveCarrusel(index) {
+        const itemWidth = items[0].clientWidth + 20; 
+        carrusel.scrollTo({
+            left: index * itemWidth,
+            behavior: 'smooth'
+        });
+    }
+
+    function next() {
+        currentIndex = (currentIndex + 1) % items.length;
+        moveCarrusel(currentIndex);
+    }
+
+    function prev() {
+        currentIndex = (currentIndex - 1 + items.length) % items.length; 
+        moveCarrusel(currentIndex);
+    }
+
+    nextButton.addEventListener('click', next);
+    prevButton.addEventListener('click', prev);
+
+    setInterval(next, 5000);
+
